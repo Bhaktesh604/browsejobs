016729cf50fee4b40280b99bca32397fbd8aad2c
Bhaktesh604 <bhakteshgadhiya604@gmail.com>
2024-12-30 09:44:30 +0530

    first

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// Counter.js
+
+function numCounter(tagId,maxCount,speed){
+    var initialNumber = 0;
+    function counter(){
+        document.getElementById(tagId).innerHTML = initialNumber;
+        ++initialNumber;
+    }
+    var counterDelay = setInterval(counter,speed);
+    function totalTime(){
+        clearInterval(counterDelay);
+    }
+    var totalPeriod = speed * (maxCount);  
+    setTimeout(totalTime, totalPeriod);
+}
+
+numCounter("Projects",634,10);
+numCounter("Clients",589,10);
+
+//
